ba453867cb845f0eb5951b740ac4d965f63f55dd
Rockheung <rockheung@gmail.com>
2022-08-13 14:40:54 +0900

    add testset

--- 01-section/01-answer.js
+++ 01-section/01-answer.js
@@ -1,4 +1,11 @@
 module.exports = function (...args) {
-  const [input] = args;
-  return Math.min(...input)
-}
+  const [arg0] = args;
+  return arg0.reduce((acc, item) => {
+    return acc > item ? item : acc;
+  });
+};
+
+module.exports.testSet = [
+  { input: [[6, 5, 11]], output: 5 },
+  { input: [[6]], output: 6 },
+];
--- test/01-section.test.js
+++ test/01-section.test.js
@@ -1,8 +1,11 @@
-const { assert, should, expect } = require("chai");
+const { expect } = require("chai");
 const answer_01 = require("../01-section/01-answer");
+
 
 describe("01-section", () => {
   it("1. 세 수 중 최솟값", () => {
-    expect(answer_01([6, 5, 11])).to.equal(5)
+    for (const { input, output } of answer_01.testSet) {
+      expect(answer_01(...input)).to.equal(output);
+    }
   });
 });

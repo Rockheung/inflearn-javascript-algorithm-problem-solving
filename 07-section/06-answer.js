e266c957a13002eb468e7ce9b187306297ba2cba
Rockheung <rockheung@gmail.com>
2022-09-11 23:18:36 +0900

    solve problem

---
+++ 07-section/06-answer.js
@@ -0,0 +1,25 @@
+module.exports = function (...args) {
+  const [[n],heights] = args;
+  let hyunSuIdx;
+  let hyunSuFriendIdx;
+  for (let i = 0; i < n - 1; i += 1) {
+    if (heights[i] >= heights[i+1]) {
+      hyunSuIdx = hyunSuIdx || i;
+      hyunSuFriendIdx = i + 1;
+      i += 2;
+    }
+    if (heights[i-1] > heights[i]) {
+      hyunSuFriendIdx = i;
+      break;
+    }
+  }
+
+  return [hyunSuIdx+1, hyunSuFriendIdx+1].join(' ')
+};
+
+module.exports.problemName = '6. 장난꾸러기 현수'
+
+module.exports.testSet = [
+  { input: [[9],[120,125,152,130,135,135,143,127,160]], output: '3 8' },
+  { input: [[6],[120,130,150,150,130,150]], output: '3 5' },
+];

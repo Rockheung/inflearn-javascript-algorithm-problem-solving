ec2334684b6c2b5ac9ab1cdd2f7d93585d353fa4
Rockheung <rockheung@gmail.com>
2022-10-09 17:10:07 +0900

    solve problem; add answer for 7-9

--- 07-section/09-answer.js
+++ 07-section/09-answer.js
@@ -1,3 +1,4 @@
+// static, O(n), but not not smart
 module.exports._ = function (...args) {
   const [[n],...friends] = args;
   let max = 0;
@@ -11,6 +12,7 @@ module.exports._ = function (...args) {
   return max;
 };
 
+// with sort method
 module.exports.__ = function (...args) {
   const [[n],...friends] = args;
   let max = 0;
@@ -30,7 +32,8 @@ module.exports.__ = function (...args) {
   return max
 }
 
-module.exports = function (...args) {
+// insertion sort
+module.exports.___ = function (...args) {
   const [[n],friend, ...friends] = args;
   let max = 0;
   let count  = 0;
@@ -65,6 +68,64 @@ module.exports = function (...args) {
     }
   }
   for (const time of times) {
+    count += time.type;
+    max = Math.max(max,count)
+  }
+  return max
+}
+
+
+// merge sort
+module.exports = function (...args) {
+  const [[n], ...friends] = args;
+  const friendsWithEnterExit = friends.map((friend) => {
+    return friend.map((time, idx) => {
+      return {
+        t: time,
+        type: idx === 0 ? 1 : -1
+      }
+    })
+  })
+  let max = 0;
+  let count  = 0;
+
+  function merge(arrA, arrB = []) {
+    let arr = [];
+    let i = 0, j = 0;
+    while(i < arrA.length && j < arrB.length) {
+      if (arrA[i].t > arrB[j].t) {
+        arr.push(arrB[j])
+        j += 1;
+      } else if (arrA[i].t === arrB[j].t){
+        if (arrA[i].type > arrB[j].type) {
+          arr.push(arrB[j]);
+          j += 1;
+        } else {
+          arr.push(arrA[i]);
+          i += 1;
+        }
+      } else {
+        arr.push(arrA[i])
+        i += 1;
+      }
+    }
+    if (i === arrA.length) {
+      arrB.slice(j).reduce((arr, itemB)=> {arr.push(itemB); return arr}, arr)
+    }
+    if (j === arrB.length) {
+      arrA.slice(i).reduce((arr, itemA)=> {arr.push(itemA); return arr}, arr)
+    }
+    return arr
+  }
+  let arr = [...friendsWithEnterExit]
+  while (arr.length > 1) {
+    let _tmpArr = [];
+    for (let i = 0; i < arr.length; i += 2) {
+      _tmpArr.push(merge(arr[i], arr[i+1]))
+    }
+    arr = _tmpArr
+  }
+  for (const time of arr[0]) {
     count += time.type;
     max = Math.max(max,count)
   }

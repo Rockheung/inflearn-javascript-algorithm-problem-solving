0f082c6f188d6ff566533ebe02fbd4580c9282e8
Rockheung <rockheung@gmail.com>
2022-10-10 18:38:17 +0900

    solve problem

--- 07-section/10-answer.js
+++ 07-section/10-answer.js
@@ -12,13 +12,19 @@ module.exports = function (...args) {
       lt = mid 
     }
   }
-  console.log(lt, rt)
-  return Math.floor((lt+ rt)/2) + 1;
+  if (numsSorted[lt] === m) {
+    return lt + 1;
+  } else if (numsSorted[rt] === m) {
+    return rt + 1;
+  }
+  return null;
 };
 
 module.exports.problemName = '10. 이분검색'
 
 module.exports.testSet = [
   { input: [[8,32],[23,87,65,12,57,32,99,81]], output: 3 },
-  { input: [[7,32],[23,87,65,57,32,99,81]], output: 2 }
+  { input: [[7,32],[23,87,65,57,32,99,81]], output: 2 },
+  { input: [[8,23],[23,87,65,12,57,32,99,81]], output: 2 },
+  { input: [[7,99],[23,87,65,57,32,99,81]], output: 7 },
 ];
